a5b29138671b0ad2b928f6367338af945d243202
Kirsty Adair <kirstyaadair@gmail.com>
2019-03-24 18:37:55 +0000

    old commit i forgot to push

--- WGE_Coursework/Assets/Scripts/InventoryScript.cs
+++ WGE_Coursework/Assets/Scripts/InventoryScript.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class InventoryScript : MonoBehaviour {
 
+    int pos = -1;
+    Vector3 pos1;
     public int dirtBlocks = 0;
     public int sandBlocks = 0;
     public int grassBlocks = 0;
@@ -13,16 +15,28 @@ public class InventoryScript : MonoBehaviour {
     public Text inventoryTextDirt;
     public Text inventoryTextSand;
     public Text inventoryTextStone;
+    public RawImage hotbarSelector;
 
 	// Use this for initialization
 	void Start () {
         DroppedCubeScript.OnEventAddInventory += AddBlockToInventory;
+        pos1 = hotbarSelector.GetComponent<RectTransform>().anchoredPosition = new Vector3(-37, 0, 0);
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float sw = Input.GetAxis("Mouse ScrollWheel");
+
+        if (sw < 0)
+        {
+            Debug.Log("Scrolling Down");
+            
+        }
+        else if (sw > 0)
+        {
+            Debug.Log("Scrolling Up");
+        }
 	}
 
     void AddBlockToInventory(int blockTex)
@@ -31,19 +45,19 @@ public class InventoryScript : MonoBehaviour {
         {
             case 1:
                 grassBlocks++;
-                inventoryTextGrass.text = "Grass blocks: " + grassBlocks;
+                inventoryTextGrass.text = grassBlocks.ToString();
                 break;
             case 2:
                 dirtBlocks++;
-                inventoryTextDirt.text = "Dirt blocks: " + dirtBlocks;
+                inventoryTextDirt.text = dirtBlocks.ToString();
                 break;
             case 3:
                 sandBlocks++;
-                inventoryTextSand.text = "Sand blocks: " + sandBlocks;
+                inventoryTextSand.text = sandBlocks.ToString();
                 break;
             case 4:
                 stoneBlocks++;
-                inventoryTextStone.text = "Stone blocks: " + stoneBlocks;
+                inventoryTextStone.text = stoneBlocks.ToString();
                 break;
         }
     }
